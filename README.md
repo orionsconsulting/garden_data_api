51175f7e8dbd6028e0a006973f683c3f143813d6
twilliams <twilliams@cedarcreek.com>
2018-06-25 17:42:37 -0500

    #8 wrote veiws for the weather station

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 source ~/PycharmProjects/Orionsconsulting/Django01/bin/activate
+
 cd ~/PycharmProjects/Orionsconsulting/Django01/garden_data_api
+
+**run tests**
+
 python3 manage.py test -v 2
 
+**run server**
+
+python3 manage.py runserver 0.0.0.0
 
 pip install --upgrade pip
 pip install django
--- garden_data/views.py
+++ garden_data/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from garden_data.submodel.weather_station import WeatherStation
+from garden_data.serializers import WeatherStationSerializer
 
-# Create your views here.
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data=data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def weather_station_list(request):
+    if request.method == 'GET':
+        weather_station = WeatherStation.objects.all()
+        weather_station_serializer = WeatherStationSerializer(weather_station, many=True)
+        return JSONResponse(weather_station_serializer.data)
+    elif request.method == 'POST':
+        weather_station_data = JSONParser.parse(request)
+        weather_station_serializer = WeatherStationSerializer(data =weather_station_data)
+        if weather_station_serializer.is_valid():
+            weather_station_serializer.save()
+            return JSONResponse(weather_station_serializer.data, status=status.HTTP_201_CREATED)
+        return JSONResponse(weather_station_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def weather_station_detail(request,pk):
+    try:
+        weather_station = WeatherStation.objects.get(pk=pk)
+    except WeatherStation.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        weather_station_serializer = WeatherStationSerializer(weather_station)
+        return JSONResponse(weather_station_serializer.data)
+
+    elif request.method == 'PUT':
+        weather_station_data = JSONParser().parse(request)
+        weather_station_serializer = WeatherStationSerializer(weather_station, data=weather_station_data)
+        if weather_station_serializer.is_valid():
+            weather_station_serializer.save()
+            return JSONResponse(weather_station_serializer.data)
+        else:
+            return JSONResponse(weather_station_serializer.errors, status==status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        weather_station.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
